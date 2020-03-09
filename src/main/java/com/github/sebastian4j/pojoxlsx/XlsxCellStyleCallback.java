4d8bd8e3e69faa9afe9dee012bbea8558979f8a0
Sebastian Avila A <sebastian4j@gmail.com>
2020-03-09 17:02:07 -0300

    corrección

--- src/main/java/com/github/sebastian4j/pojoxlsx/XlsxCellStyleCallback.java
+++ src/main/java/com/github/sebastian4j/pojoxlsx/XlsxCellStyleCallback.java
@@ -14,7 +14,7 @@ public interface XlsxCellStyleCallback {
 	 * 
    * @param col indice de la columna del documento que está en proceso (comienza desde 0)
 	 * @param row indice de la fila que está en proceso
-   * @param font fuente a la que se le aplican los estulos
+   * @param font fuente a la que se le aplican los estilos
    * @param val valor que está siendo procesado (valores no nulos)
    */
   boolean apply(final int col, final int row, final XSSFFont font, final Object val);
